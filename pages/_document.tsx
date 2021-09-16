e8cbac7744789635da84713c23f1c6a64018202c
Stefan Golban <escgamingt.o@gmail.com>
2021-09-16 21:46:40 +0300

    fix: format & lint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-document-import-in-page */
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
@@ -11,8 +11,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
